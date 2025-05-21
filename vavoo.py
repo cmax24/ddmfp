9fb78d996ef568df9683f945328c3b231e0cc3f5
cmax24 <cmax24@hotmail.it>
2025-05-21 16:09:53 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "cmax24"
+BRANCHEPG = "ddmfp"
+MFPLINK = "https://cmax24-dandy.hf.space"     # non mettere lo / finale al link
+MFPPSW = "154233"
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
